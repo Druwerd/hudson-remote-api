ca38dedcf2693440e449f1ba7f53c45745e15c12
Drew <drew@dru-ibarras-macbook.local>
2010-07-28 20:41:53 -0700

    refactored BuildQueue

--- lib/build_queue.rb
+++ lib/build_queue.rb
@@ -5,20 +5,14 @@ module Hudson
         # List the jobs in the queue
         def self.list()
             path = "#{HUDSON_URL_ROOT}/queue/api/xml"
-            uri = URI::HTTP.build(:host => @@host, :path => path, :port => @@port)
-            response = Net::HTTP.get_response(uri)
-            
-            if response.is_a?(Net::HTTPSuccess) or response.is_a?(Net::HTTPRedirection)
-                queue = []
-                queue_doc = REXML::Document.new(response.body)
-                return queue if queue_doc.elements["/queue/item"].nil?
-                queue_doc.each_element("/queue/item/task") do |job|
-                    queue << job.elements["name"].text
-                end
-                queue
-            else
-                raise HudsonApiError, "Error retrieving build queue"
+            xml = get_xml(path)
+            queue = []
+            queue_doc = REXML::Document.new(xml)
+            return queue if queue_doc.elements["/queue/item"].nil?
+            queue_doc.each_element("/queue/item/task") do |job|
+                queue << job.elements["name"].text
             end
+            queue
         end
     end
 end
