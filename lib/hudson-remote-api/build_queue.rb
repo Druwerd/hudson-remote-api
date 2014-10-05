b7edc2d3d6bb06db32dd25e741513cbe1356726d
Dru Ibarra <Druwerd@gmail.com>
2014-10-04 17:18:58 -0700

    better comment

--- lib/hudson-remote-api/build_queue.rb
+++ lib/hudson-remote-api/build_queue.rb
@@ -7,11 +7,10 @@ module Hudson
         @@xml_api_build_queue_info_path = File.join(Hudson[:url], "queue/api/xml")
       end
 
-      # List the jobs in the queue
       def list
         xml = get_xml(@@xml_api_build_queue_info_path)
         queue_doc = REXML::Document.new(xml)
-        return [] if queue_doc.elements["/queue/item"].nil?
+        return [] if queue_doc.elements["/queue/item"].nil? # there's nothing in the queue
 
         queue_doc.each_element("/queue/item/task").collect{ |job| job.elements["name"].text }
       end
