335a39ebd868e599fe5aa3fbdc334a516f429b60
Dru Ibarra <Druwerd@gmail.com>
2014-10-04 17:26:42 -0700

    separate class methods

--- lib/hudson-remote-api/config.rb
+++ lib/hudson-remote-api/config.rb
@@ -11,42 +11,44 @@ module Hudson
     :proxy_host => nil,
     :proxy_port => nil
   }
-  
-  def self.[](param)
-    return @@settings[param]
-  end
 
-  def self.[]=(param,value)
-    param = param.to_sym if param.kind_of?(String)
-    if param == :host or param == :url
-      value = "http://#{value}" if value !~ /https?:\/\//
-      @@settings[:url] = value
-    else
-      @@settings[param]=value
+  class << self
+    def [](param)
+      return @@settings[param]
     end
-    HudsonObject::load_xml_api
-    BuildQueue::load_xml_api
-  end
 
-  def self.settings=(settings)
-    if settings.kind_of?(Hash)
-      settings.each do |param, value|
-        Hudson[param] = value
+    def []=(param,value)
+      param = param.to_sym if param.kind_of?(String)
+      if param == :host or param == :url
+        value = "http://#{value}" if value !~ /https?:\/\//
+        @@settings[:url] = value
+      else
+        @@settings[param]=value
+      end
+      HudsonObject::load_xml_api
+      BuildQueue::load_xml_api
+    end
+
+    def settings=(settings)
+      if settings.kind_of?(Hash)
+        settings.each do |param, value|
+          Hudson[param] = value
+        end
       end
     end
-  end
-  
-  # Discovers nearby Hudson server on the network and configures settings
-  def self.auto_config
-    xml_response = discover()
-    if xml_response
-      doc = REXML::Document.new(xml_response)
-      url = doc.elements["/hudson/url"]
-      if url
-        Hudson[:url] = url.text
-        Hudson[:version] = doc.elements["/hudson/version"].text
-        puts "found Hudson version #{Hudson[:version]} @ #{Hudson[:url]}"
-        return true
+    
+    # Discovers nearby Hudson server on the network and configures settings
+    def auto_config
+      xml_response = discover()
+      if xml_response
+        doc = REXML::Document.new(xml_response)
+        url = doc.elements["/hudson/url"]
+        if url
+          Hudson[:url] = url.text
+          Hudson[:version] = doc.elements["/hudson/version"].text
+          puts "found Hudson version #{Hudson[:version]} @ #{Hudson[:url]}"
+          return true
+        end
       end
     end
   end
