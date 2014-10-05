9e32daad44b94bfef2e73b5997ebe2e7600ebc94
Dru Ibarra <Druwerd@gmail.com>
2014-10-04 17:11:41 -0700

    separate class methods

--- lib/hudson-remote-api/build_queue.rb
+++ lib/hudson-remote-api/build_queue.rb
@@ -1,23 +1,26 @@
 module Hudson
   # This class provides an interface to Hudson's build queue
   class BuildQueue < HudsonObject
-
-    def self.load_xml_api
-      @@xml_api_build_queue_info_path = File.join(Hudson[:url], "queue/api/xml")
-    end
-
-    load_xml_api
-
-    # List the jobs in the queue
-    def self.list()
-      xml = get_xml(@@xml_api_build_queue_info_path)
-      queue = []
-      queue_doc = REXML::Document.new(xml)
-      return queue if queue_doc.elements["/queue/item"].nil?
-      queue_doc.each_element("/queue/item/task") do |job|
-        queue << job.elements["name"].text
+    
+    class << self
+      def load_xml_api
+        @@xml_api_build_queue_info_path = File.join(Hudson[:url], "queue/api/xml")
       end
-      queue
+
+      # List the jobs in the queue
+      def list
+        xml = get_xml(@@xml_api_build_queue_info_path)
+        queue = []
+        queue_doc = REXML::Document.new(xml)
+        return queue if queue_doc.elements["/queue/item"].nil?
+        queue_doc.each_element("/queue/item/task") do |job|
+          queue << job.elements["name"].text
+        end
+        queue
+      end
     end
+    
+    load_xml_api
   end
+
 end
